d153a6cba852608862cc5c1af2f68e0fe79998f4
eliyagalis <eliyagalisbiz@gmail.com>
2024-12-11 13:36:19 +0200

    finishing project api

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -1,40 +1,96 @@
+import Project from "../models/Project"
 
-export const getProjects = async ()=> {
+export const getProjects = async (req,res)=> {
     try {
-        
+        const projects = await Project.find({});
+        res.status(200).json(projects);
+    
     } catch (error) {
-        console.log("couldn't load the server", error)
+        console.log("Error in get request", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
-export const getProject = async ()=> {
+export const getProjectById = async (req,res)=> {
     try {
+        const { projectId  } = req.params;
+
+        if (!projectId){
+            return res.status(400).json({error: "Id is not valid"});
+        }
         
+        const requestedProject = await Project.findById(projectId );
+        
+        if(!requestedProject){
+            return res.status(404).json({error: "Project was not found"});
+        }
+        
+        res.status(200).json(requestedProject);
+
     } catch (error) {
-        console.log("couldn't load the server", error)
+        console.log("Error in get request id", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
-export const addProject = async ()=> {
+export const createProject = async (req,res)=> {
     try {
-        
+        const { name, ...otherFields } = req.body;
+
+        if (!name) {
+            return res.status(400).json({error: "Project name is required"});
+        }
+
+        const newProject = new Project({
+            name,
+            ...otherFields
+        })
+
+        await newProject.save();
+
+        res.status(201).json(newProject);
+
     } catch (error) {
-        console.log("couldn't update the server", error)
+        console.log("Error in creating", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
-export const updateProject = async ()=> {
+export const updateProject = async (req,res)=> {
     try {
+        const { id } = req.params;
+        const updatedProject = await Project.findByIdAndUpdate(id, req.body, { new: true });
         
+        if (!updatedProject) {
+            return res.status(404).json({ error: "Project not found" });
+        }
+
+        res.status(200).json(updatedProject);
+
     } catch (error) {
-        console.log("couldn't update the server", error)
+        console.log("Error in updating", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
-export const deleteProject = async ()=> {
+export const deleteProject = async (req,res)=> {
     try {
-        
+        const { id } = req.params.id;
+
+        if (!id){
+            return res.status(400).json({error: "Id is not valid"});
+        }
+
+        const result = await Project.findByIdAndDelete(id);
+
+        if (!result){
+            return res.status(404).json({error: "Project not found"});
+        }
+
+        return res.status(200).json({ message: "Project deleted successfully"});
+
     } catch (error) {
-        console.log("couldn't update the server", error)
+        console.log("Error in deleting", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
--- server/routes/projectsRouter.js
+++ server/routes/projectsRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getProjects } from '../controllers/projectsController.js';
+import { createProject, deleteProject, getProjectById, getProjects, updateProject } from '../controllers/projectsController.js';
 
 const projectsRouter = express.Router();
 
-projectsRouter.route('/').get(getProjects).post().put().delete();
+projectsRouter.route('/').get(getProjects).post(createProject);
 
-
+projectsRouter.route('/:id')
+    .get(getProjectById)
+    .put(updateProject)
+    .delete(deleteProject);
 
 export default projectsRouter
